9405eff985279ae1e6278c6e654b92f938d50db6
Sanghoon Shin <sanghoonshin@Orca-MacBook.local>
2020-07-20 17:25:46 +0900

    #0.4 First steps with Typescript

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Hoon", age = 24, gender = "male";
+// 물음표를 넣어서 parameter 가 선택적이라고 알려줄 수 있다.
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you're ${age}, you are a ${gender}`);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
