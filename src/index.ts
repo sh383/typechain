1223063e87114660b9541dce061ead5624e070d1
Sanghoon Shin <sanghoonshin@Orca-MacBook.local>
2020-07-21 17:12:48 +0900

    #0.6 Interfaces on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
-// ? 를 넣어서 parameter 가 선택적이라고 알려줄 수 있다. const sayHi = (name, age, gender?)
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, you're ${age}, you are a ${gender}!`;
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "Hoon",
+  age: 22,
+  gender: "male",
 };
 
-console.log(sayHi("Hoon", 24, "male"));
+// ? 를 넣어서 parameter 가 선택적이라고 알려줄 수 있다. const sayHi = (name, age, gender?)
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you're ${person.age}, you are a ${person.gender}!`;
+};
+
+console.log(sayHi(person));
 // 안하면 name 을 정의할 수 없다
 export {};
