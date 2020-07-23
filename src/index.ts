458343676b661883b4818b2697f735e71b79d242
Sanghoon Shin <sanghoonshin@Orca-MacBook.local>
2020-07-23 12:13:16 +0900

    #9 Creating a Block part 3

--- src/index.ts
+++ src/index.ts
@@ -37,11 +37,31 @@ const genesisBlock: Block = new Block(0, "23202020", "", "Hello", 123456);
 // blockchain 에 Block 만 추가하도록
 let blockchain: Block[] = [genesisBlock];
 
-console.log(blockchain);
-
 const getBlockchain = (): Block[] => blockchain;
 
 const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
 const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimeStamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimeStamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimeStamp
+  );
+  return newBlock;
+};
+
+console.log(createNewBlock("Hello"));
 export {};
